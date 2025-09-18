183038617fd62931e56ef81afbafa1f9a38cc1f7
adrianosiqueira <as.adriano.siqueira@gmail.com>
2025-09-18 12:07:28 -0300

    Remover TelaLoginHelper.

--- src/main/java/controller/TelaLoginController.java
+++ src/main/java/controller/TelaLoginController.java
@@ -1,7 +1,6 @@
 
 package controller;
 
-import controller.helper.TelaLoginHelper;
 import java.sql.Connection;
 import model.DAO.LoginDAO;
 import model.Login;
@@ -9,24 +8,23 @@ import view.TelaLogin;
 import java.sql.SQLException;
 import javax.swing.JOptionPane;
 
-public class TelaLoginController 
+public class TelaLoginController
 {
 
     //controllador que vai efetuar a autenticação do usuario
-    
+
     private TelaLogin view;
     //controllador que vai obter ou setar o modelo
-    private TelaLoginHelper helper;
+
 
     public TelaLoginController(TelaLogin view) {
         this.view = view;
-        this.helper = new TelaLoginHelper(view);
-    }    
-    
+    }
+
     //metodo autenticar
     public void autenticar() throws SQLException
-    {      
-        
+    {
+
         //comparações logicas
         if(view.txtLogin.getText().trim().isEmpty() && view.txtSenha.getText().trim().isEmpty())
         {
@@ -45,33 +43,43 @@ public class TelaLoginController
 
             JOptionPane.showMessageDialog(null, "Campo senha esta vazio!","ATENÇÃO", JOptionPane.WARNING_MESSAGE);
 
-        }          
-        
+        }
+
         //Busca usuario da view
-        Login login = helper.obterModelo();
-        
+        Login login = obterModelo();
+
         //verifica se existe no banco de dados
         //chama loginDAO
         Connection conexao = new Conexao().Conectar();
         LoginDAO loginDAO = new LoginDAO(conexao);
-        
+
         //existe loginAutenticar no banco por pesquisarUsuarioSenha?
         boolean existe = loginDAO.pesquisarUsuarioSenha(login);
-        
+
         //se existir direciona para tela principal
         if(existe)
         {
             //mensagem no console informando que o usuario existe
             System.out.println("Usuario existe!");
-            
+
         }
         else//senao
         {
-        
+
         JOptionPane.showMessageDialog(null, "Usuario e/ou senha invalidos, ou não existe no banco de dados!","ATENÇÃO", JOptionPane.WARNING_MESSAGE);
-            
-        }    
-        
-    }        
+
+        }
+
+    }        public Login obterModelo()
+{
+
+    String usuario = view.getTxtLogin().getText();//string usuario recebe view pegue gettxtlogin e pegue o texto
+    String senha = view.getTxtSenha().getText();//string senha recebe view pegue gettxtsenha e pegue o texto
+    //construir objeto do tipo login
+    Login login = new Login(usuario,senha);
+
+    return login;
+
+}
 
 }
